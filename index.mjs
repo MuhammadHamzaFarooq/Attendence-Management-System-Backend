34ae70a2613d4074cda3052665805005011b63f0
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-02-08 19:30:29 +0500

    update

--- index.mjs
+++ index.mjs
@@ -6,7 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 // let URI = `mongodb+srv://admin:12345@nodejscluster01.u7jbf.mongodb.net/attendenceDB?retryWrites=true&w=majority`;
 
-
 // call middleware
 app.use(express.json());
 app.use(cors());
@@ -47,17 +46,85 @@ const studentSchema = mongoose.Schema({
 
 const Student = mongoose.model("Student", studentSchema);
 
+const attendenceSchema = mongoose.Schema({
+  id: {
+    type: String,
+    isRequired: true,
+  },
+  attendence: {
+    type: String,
+    isRequired: true,
+  },
+  date: {
+    type: String,
+    isRequired: true,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Attendence = mongoose.model("Attendence", attendenceSchema);
+
+// post Request student
+app.post("/attendence", (req, res) => {
+  const { id, attendence, date } = req.body;
+
+  let attendenceStd = {
+    id,
+    attendence,
+    date,
+  };
+
+  let studentAttendence = new Attendence(attendenceStd);
+
+  studentAttendence.save((Error, Save) => {
+    if (!Error) {
+      return res.send("Student Attendence Add In DB Successfully");
+    } else {
+      return res.status(400).send("Something Went Wrong 🤦‍♂️");
+    }
+  });
+});
+
 app.get("/", (req, res) => {
   res.send("Attendence Management System Server Online");
 });
 
-// post Request
-app.post("/student", (req, res) => {
-  res.send("Post Request "+req.body);
-  console.log(req.body)
-
+// Get All Students
+app.get("/students", (req, res) => {
+  Student.find({}, (err, data) => {
+    if (!err) {
+      res.send(data);
+    } else {
+      res.status(500).send("Something went wrong 😓");
+    }
+  });
 });
 
+// post Request student
+app.post("/student", (req, res) => {
+  const { Name, Course, RollNo, Semester, Batch } = req.body;
+
+  let std = {
+    Name,
+    Course,
+    RollNo,
+    Semester,
+    Batch,
+  };
+
+  let newStduent = new Student(std);
+
+  newStduent.save((Error, Save) => {
+    if (!Error) {
+      return res.send("Student Add In DB Successfully");
+    } else {
+      return res.status(400).send("Something Went Wrong 🤦‍♂️");
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
